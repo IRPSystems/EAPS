ca7e1d329cef4c3eaf6fd8b1a79f597bba0116b2
Smadar Tsdaka <smadar@irpsystems.com>
2024-02-19 15:43:15 +0200

    Add solution

---
+++ EAPS/ViewModels/EAPSMainViewModel.cs
@@ -0,0 +1,24 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Reflection;
+
+namespace EAPS.ViewModels
+{
+	public class EAPSMainViewModel: ObservableObject
+	{
+		#region Properties
+
+		public string Version { get; set; }
+
+		#endregion Properties
+
+		#region Constructor
+
+		public EAPSMainViewModel()
+		{
+			Version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+		}
+
+		#endregion Constructor
+	}
+}
