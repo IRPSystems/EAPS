febc21ace86ccff1ccb406c67f256f490df0e080
Smadar Tsdaka <smadar@irpsystems.com>
2024-02-21 16:24:25 +0200

    Add SettingsView

---
+++ EAPS/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,17 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using DeviceHandler.Models;
+using DeviceHandler.ViewModels;
+
+namespace EAPS.ViewModels
+{
+	public class SettingsViewModel: ObservableObject
+	{
+		public CommunicationViewModel CommunicationSettings { get; set; }
+
+		public SettingsViewModel(DevicesContainer devicesContainer)
+		{
+			CommunicationSettings = new CommunicationViewModel(devicesContainer);
+		}
+	}
+}
